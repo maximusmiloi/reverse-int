7e8893e6385b7c0c0a3051deae295102ce9e599f
maximusmiloi <maximusmiloi@gmail.com>
2022-02-23 23:27:38 +0300

    feat: test complited

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+	let result = [];
+	let b = (Math.abs(n) + '').split('');
+	let newArr = b.forEach(el => result.unshift(el));
+	return +result.join('');
 }
